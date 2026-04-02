193962ca0c55eb62e55adb47b4aeadc843e9eec3
phatgia <phatgia24112006@gmail.com>
2026-04-03 01:29:39 +0700

    feat: add Models, Factory, Migrations, Controller(Update later)

---
+++ app/Models/Note.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class Note extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'image_path',
+        'password',
+    ];
+    protected $hidden = [
+        'password',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}

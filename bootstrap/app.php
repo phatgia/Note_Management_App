8fb1cf5567fe183c2b8483c53da67ebc11981f54
phatgia <phatgia24112006@gmail.com>
2026-04-27 19:29:48 +0700

    fix: trust proxies to resolve mixed content HTTPS errors on Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ['middleware' => ['web', 'auth']], 
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
         $middleware->redirectUsersTo('/home');
         $middleware->web(append: [
